5e4ee54bdcf6b99e5b1fc180a31845d7fec4f517
lednovanna <lelagency@gmail.com>
2023-10-18 23:15:11 +0100

    lesson4-5

--- src/route/index.js
+++ src/route/index.js
@@ -120,6 +120,13 @@ router.get('/skills', function (req, res) {
         point: 0,
       },
     ],
+
+    hobbies: [
+      { name:'Dancing', isMaine: true },
+       { name: 'Tennis', isMaine: true },
+       { name:'reading' , isMain: false },
+     ],
+
   },
 
    footer,
@@ -129,6 +136,88 @@ router.get('/skills', function (req, res) {
 
 
 // ==============================================================================
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+    title: 'Ivan Ivanov | Work',
+  },
+
+  header,
+
+    main:{
+      work: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+          name: 'it-brains',
+          url: 'htttp//it-brains.com.ua',
+          },
+
+        duration: {
+          from: '10.10.2023',
+          to: '22.03.2023'
+        },
+        }, 
+        
+        {
+          position: 'Junior Fullstack Developer',
+          company: { 
+            name: 'CanShecode',
+          url:'https//canshecode.com.ua',
+          },
+
+        duration: {
+          from: '10.10.2023',
+          to: null,
+        },
+
+        projectAmount: 3,
+
+        projects: [
+          {
+            name: 'Resume',
+            url: 'http//resume.com.ua',
+            about: 'інформація про проект',
+            stacks: [
+              {
+                name: 'React.js',
+              },
+              {
+                name: 'HTML/CSS',
+              },
+              {
+                name: 'Node.js',
+              },
+            ],
+            awards: [
+              {
+                name: 'Background verification...',
+              },
+              {
+                name: 'Preparing SEO...',
+              },
+            ],
+            stackAmount: 2,
+            awardAmount: 2,
+          },
+        ],
+      },
+       ] ,
+},
+
+   footer,
+
+    })
+})
+
+
+//================================================================================================
+
 router.get('/education', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('education', {
@@ -140,32 +229,128 @@ router.get('/education', function (req, res) {
 
   header,
 
-
+    main:{
       education: [
-        'Kyiv Polytechnic University',
-        'Computer Academy',
-        'CanSheCode Shool',
+       { name:'Kyiv Polytechnic University', isEnd: true },
+        { name: 'Computer Academy', isEnd: true },
+        { name:'CanSheCode Shool' , isEnd: false },
       ],
 
-    summary: {
-      title: 'Summary',
-      text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
-      work on a new project I learn the domain and try to understand the idea of the project. Good team
-      player, every colleague is a friend to me.`
-    },
-
-    experience: {
-      title: 'Other experience',
-      text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
-        tournament position, goals etc), analyzing by simple mathematics models and preparing probability
-        for such events like: money line - first win / draw / second win, totals etc.`
-    }
-   },
+      certificates: [
+        { name:'CanShecode HTML', id: 111111 },
+        { name: 'CnaShecode JS', id: 22222 },
+        { name:'IT-Brains React' , id: 33333 },
+      ],   
+},
 
    footer,
-    
-  )
+
+    })
 })
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/person', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('person', {
+    layout: 'person',
+
+    person: {
+      name: 'Emma Johnson',
+      age: 32,
+      gender: 'Female',
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '10001',
+        country: 'USA',
+      },
+      education: [
+        {
+          degree: 'Bachelor of Science',
+          major: 'Computer Science',
+          university:
+            'Massachusetts Institute of Technology',
+          graduationYear: 2012,
+        },
+      ],
+      workExperience: [
+        {
+          company: 'Google',
+          title: 'Software Engineer',
+          startDate: '2012-06-01',
+          endDate: '2016-12-31',
+          responsibilities: [
+            'Developed new features for Google Maps',
+            'Worked on improving search algorithms',
+          ],
+          year_founded: 1990,
+          industry: 'Technology',
+          employees: [
+            {
+              name: 'John Smith',
+              position: 'CEO',
+              department: 'Executive',
+              projects: [
+                {
+                  name: 'Project Alpha',
+                  description:
+                    'Developing new software platform',
+                  status: 'In Progress',
+                  teams: [
+                    {
+                      team_name: 'Awesome Team',
+                      team_leader: {
+                        name: 'John Smith',
+                        title: 'Team Leader',
+                        email: 'john.smith@example.com',
+                      },
+                      team_members: [
+                        {
+                          name: 'Alice Johnson',
+                          title: 'Software Engineer',
+                          email:
+                            'alice.johnson@example.com',
+                          skills: ['Java', 'Python', 'SQL'],
+                          projects: [
+                            {
+                              name: 'Project A',
+                              description:
+                                'Lorem ipsum dolor sit amet',
+                              technologies: [
+                                'Java',
+                                'Spring Framework',
+                              ],
+                              team_members: [
+                                {
+                                  name: 'Bob Lee',
+                                  title:
+                                    'Software Engineer',
+                                },
+                                {
+                                  name: 'Cindy Chen',
+                                  title: 'UI Designer',
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  })
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
